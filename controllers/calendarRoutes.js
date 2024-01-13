1cef18e930a2adc06f238d4e337595c1be75571d
LovepreetSingh24 <lovepreet.singh@att.net>
2024-01-12 21:29:56 -0800

    deleted unnecessary files
    deleted files and some code that were going to be used for other pages

--- controllers/calendarRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Route for month view
-router.get('/month', (req, res) => {
-  res.render('calendar/month-view', { /* pass any necessary data */ });
-});
-
-module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,9 @@
 const router = require('express').Router();
-const calendarRoutes = require('./calendarRoutes');
-const dayViewRoutes = require('./dayViewRoutes');
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/calendar', calendarRoutes);
-router.use('/day', dayViewRoutes);
 
 module.exports = router;
